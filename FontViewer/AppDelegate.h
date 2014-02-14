8e67028cb6aa8becf09bf926c122ecf8619f0d21
Jens Schwarzer <schwarzer@schwarzer.dk>
2014-02-14 10:19:38 +0100

    Initial Commit

---
+++ FontViewer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FontViewer
+//
+//  Created by Jens Schwarzer on 14/02/14.
+//  Copyright (c) 2014 marchv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
